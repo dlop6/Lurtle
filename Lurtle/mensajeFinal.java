38a68920a79892f10e605e7f1964ea0d8164b05d
dlop6 <lop23747@uvg.edu.gt>
2023-08-04 23:14:02 -0600

    si

--- Lurtle/mensajeFinal.java
+++ Lurtle/mensajeFinal.java
@@ -2,6 +2,6 @@ import greenfoot.*; // Asegúrate de importar la librería Greenfoot
 
 public class mensajeFinal extends Actor {
     public mensajeFinal() {
-        setImage(new GreenfootImage("¡Juego finalizado!", 36, Color.WHITE, new Color(0, 0, 0, 0)));
+        setImage(new GreenfootImage("¡Juego finalizado! Presione 'r' para reiniciar", 36, Color.WHITE, new Color(0, 0, 0, 0)));
     }
 }
